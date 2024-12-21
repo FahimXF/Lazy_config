8827d93e8f0467bde06ed7825e61be122263bf89
FahimXF <shahriarfahim105@gmail.com>
2024-12-21 12:55:31 +0600

    V2
    Ufo,keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,22 +2,67 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+local opts = { noremap = true, silent = true }
+local keymap = vim.keymap.set
+
 -- motion keys (left, down, up, right serially)
 
 -- Movement remaps
-vim.keymap.set({ "n", "v", "o" }, "j", "h", { desc = "Move left" })
-vim.keymap.set({ "n", "v", "o" }, "k", "k", { desc = "Move up" }) -- No change for 'k'
-vim.keymap.set({ "n", "v", "o" }, "l", "j", { desc = "Move down" })
-vim.keymap.set({ "n", "v", "o" }, ";", "l", { desc = "Move right" })
+keymap({ "n", "v", "o" }, "j", "h", { desc = "Move left" })
+keymap({ "n", "v", "o" }, "k", "k", { desc = "Move up" }) -- No change for 'k'
+keymap({ "n", "v", "o" }, "l", "j", { desc = "Move down" })
+keymap({ "n", "v", "o" }, ";", "l", { desc = "Move right" })
 
 -- buffers
-vim.keymap.set("n", "<tab>", "<cmd>bprevious<cr>", { desc = "Prev Buffer" })
-vim.keymap.set("n", "<S-tab>", "<cmd>bnext<cr>", { desc = "Next Buffer" })
-vim.keymap.set("n", "<C-tab>", "<cmd>e #<cr>", { desc = "Switch to Other Buffer" })
-vim.keymap.set("n", "<leader>x", function()
+keymap("n", "<tab>", "<cmd>bprevious<cr>", { desc = "Prev Buffer" })
+keymap("n", "<S-tab>", "<cmd>bnext<cr>", { desc = "Next Buffer" })
+keymap("n", "<C-tab>", "<cmd>e #<cr>", { desc = "Switch to Other Buffer" })
+keymap("n", "<leader>x", function()
   Snacks.bufdelete()
 end, { desc = "Delete Buffer" })
-vim.keymap.set("n", "<leader>X", function()
+keymap("n", "<leader>X", function()
   Snacks.bufdelete.other()
 end, { desc = "Delete Other Buffers" })
-vim.keymap.set("n", "<leader><C-x>", "<cmd>:bd<cr>", { desc = "Delete Buffer and Window" })
+keymap("n", "<leader><C-x>", "<cmd>:bd<cr>", { desc = "Delete Buffer and Window" })
+
+-- better window navigation
+keymap("n", "<C-j>", "<C-w>h", { desc = "Move to left window" })
+keymap("n", "<C-l>", "<C-w>j", { desc = "Move to down window" })
+keymap("n", "<C-k>", "<C-w>k", { desc = "Move to up window" })
+keymap("n", "<C-;>", "<C-w>l", { desc = "Move to right window" })
+
+-- move text up and down
+keymap("v", "<A-l>", ":m '>+1<CR>gv=gv", { desc = "Move text down", noremap = true, silent = true })
+keymap("v", "<A-k>", ":m '<-2<CR>gv=gv", { desc = "Move text up", noremap = true, silent = true })
+keymap("v", "p", '"_dP', { desc = "Paste without yanking", noremap = true, silent = true })
+keymap("n", "<A-l>", ":m .+1<CR>==", { desc = "Move line down", noremap = true, silent = true })
+keymap("n", "<A-k>", ":m .-2<CR>==", { desc = "Move line up", noremap = true, silent = true })
+keymap("x", "L", ":m '>+1<CR>gv=gv", { desc = "Move selection down", noremap = true, silent = true })
+keymap("x", "K", ":m '<-2<CR>gv=gv", { desc = "Move selection up", noremap = true, silent = true })
+keymap("x", "<A-l>", ":m '>+1<CR>gv=gv", { desc = "Move selection down", noremap = true, silent = true })
+keymap("x", "<A-k>", ":m '<-2<CR>gv=gv", { desc = "Move selection up", noremap = true, silent = true })
+
+-- better delete and yank
+keymap({ "n", "v" }, "d", '"_d', { desc = "Delete without yanking", noremap = true, silent = true })
+keymap("n", "x", '"_x', { desc = "Delete character without yanking", noremap = true, silent = true })
+
+-- repeat previous f,t,F,T command
+
+vim.keymap.set("n", "'", ";")
+
+--paste without overwriting
+
+vim.keymap.set("n", "p", "P")
+
+-- redo
+
+vim.keymap.set("n", "U", "<C-r>")
+
+----skip folds (up, down)
+--
+--vim.cmd("nmap k gk")
+--vim.cmd("nmap l gj")
